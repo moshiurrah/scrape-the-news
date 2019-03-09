5cf0ddc0482a56586b7395062b388eee21ec5a92
Mohammad Moshiur Rahman <43156585+moshiurrah@users.noreply.github.com>
2019-03-09 11:17:37 -0500

    Update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# scrape-the-news
+# Scrape the news
+This App was created to scrape the most current articles from the ARTNEWS.
+
+Technologies used:
+. Node.js
+. Express.js
+. MongoDB
+. Mongoose
+. Handlebars.js
+. Javascript
+. AJAX/JSON
+NPM modules:
+. Cheerio
+. axios
---
+++ public/assets/js/mainpage.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".close").click(function() {
+    $(this)
+      .parents(".modal")
+      .css("display", "none");
+  });
+
+  $("#clear-btn").click(function(event) {
+    event.preventDefault();
+    $.get("/clearall").then(function(data) {
+      console.log(data);
+      location.reload();
+    });
+  });
+});
